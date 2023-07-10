56b00f6178d2b9040ab1cf3ac2c0b4f7a9ab2c6c
diata0210 <128179434+diata0210@users.noreply.github.com>
2023-07-10 22:34:58 +0700

    add StoreRevenueModelView

---
+++ ProjectDatabase/Models/StoreRevenueModelView.cs
@@ -0,0 +1,9 @@
+namespace ProjectDatabase.Models
+{
+    public class StoreRevenueModelView
+    {
+        public List<Store> ? Stores { get; set; }
+        public List<Revenue> ? Revenues { get; set; }
+    }
+
+}
