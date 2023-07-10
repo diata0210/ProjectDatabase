c1770bab3d0fa0304aa5fe08ccda28c11051c909
diata0210 <128179434+diata0210@users.noreply.github.com>
2023-07-10 22:33:03 +0700

    add revenues controller

---
+++ ProjectDatabase/Controllers/RevenuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazorise.Licensing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters.Xml;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectDatabase.Models;
+
+namespace ProjectDatabase.Controllers
+{
+    public class RevenuesController : Controller
+    {
+        private readonly OrderDbContext _context;
+
+        public RevenuesController(OrderDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Revenues
+        public async Task<IActionResult> Index()
+        {
+            var orderDbContext = _context.Revenues.Include(r => r.Store);
+
+            var newlist = new StoreRevenueModelView
+            {
+                Stores = await _context.Stores.ToListAsync(),
+                Revenues = await orderDbContext.ToListAsync()
+            };
+            return View(newlist);
+        }
+
+    }
+}
